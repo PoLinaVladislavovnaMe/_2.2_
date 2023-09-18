d0c416ffd9138684a5b202d00a5c358ae916a53f
Polina Moiseenko <trylyalya@rambler.ru>
2023-09-18 19:01:11 +0700

    Reformate code

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int capital = 100; // Первоначальный капитал
         int replenishment = 300; // Сумма пополнения
-        int bonus ;
+        int bonus;
         if (replenishment >= 1000) {
             bonus = replenishment / 100;
         } else {
-            bonus =0;}
+            bonus = 0;
+        }
         int score = capital + bonus + replenishment; // Итоговый счет
-        System.out.println( "Ваши бонусы составляют " + score);
+        System.out.println("Ваши бонусы составляют " + score);
 
 
     }
